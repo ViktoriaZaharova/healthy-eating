14e74b558b473be293fa05b69acaa71a86bdb56e
Viktoria <zaharova181114@gmail.com>
2022-04-01 00:47:59 +0300

    image

--- app/js/common.js
+++ app/js/common.js
@@ -5,3 +5,23 @@ $('.actual-quote-slider').slick({
     prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
 });
+
+$('.home-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    dots: true,
+    appendDots: '.home-slider__nav',
+    appendArrows: '.home-slider__nav',
+    prevArrow: '<button type="button" class="slick-prev slick-arrow-border"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next slick-arrow-border"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+});
+
+$('.btn-burger').on('click', function (e) {
+    e.preventDefault();
+    $('.mobile-menu').fadeToggle();
+});
+$('.btn-close').on('click', function (e) {
+    e.preventDefault();
+    $('.mobile-menu').fadeOut();
+});
+
