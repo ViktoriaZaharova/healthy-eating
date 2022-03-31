79156486e5e20415ea95e7a9501b26fba881db75
Viktoria <zaharova181114@gmail.com>
2022-03-31 13:05:01 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,7 @@
+$('.actual-quote-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    appendArrows: '.actual-quote-slider__nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+});
