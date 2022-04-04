2d2fa8b706bb926b13318d1f3e4d1439544b94dc
Viktoria <zaharova181114@gmail.com>
2022-04-04 14:52:16 +0300

    edit1

--- app/js/common.js
+++ app/js/common.js
@@ -35,4 +35,21 @@ $('.btn-close-sidebar').on('click', function (e) {
     $('.sidebar').fadeOut();
 });
 
+$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+    if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    }
+    var $subMenu = $(this).next(".dropdown-menu");
+    $subMenu.toggleClass('show');
+
+
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+        $('.dropdown-submenu .show').removeClass("show");
+    });
+
+
+    return false;
+});
+
 new WOW().init();
+
